66903d0d95e6091ab6a029c9255f2b7046355ce2
Gert Hengeveld <info@ghengeveld.nl>
2015-11-20 11:03:02 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# npm-scripts-watch
-Globbing file watcher to automatically run npm scripts.
+# npm-scripts-watcher
+
+Globbing file watcher to automatically run npm scripts from package.json when files change, with pretty colors.
+
+Example output:
+![Screenshot](https://raw.github.com/wehkamp/npm-scripts-watcher/master/screenshot.png)
+
+## Usage
+
+First install it as a devDependency:
+
+```
+npm i -D npm-scripts-watcher
+```
+
+Add a top-level `watch` config to your package.json and a `watch` script to your `scripts`:
+
+```json
+{
+  "scripts": {
+    "test": "mocha",
+    "watch": "npm-scripts-watcher"
+  },
+  "watch": {
+    "{src,test}/**/*.js": ["test"]
+  }
+}
+```
+
+Keys in the `watch` object should be [globs](https://www.npmjs.com/package/glob) and values should be an array of script
+names as specified in `scripts`.
+
+Now you can start the file watcher using `npm run watch`.
---
+++ src/watcher.js
@@ -0,0 +1,46 @@
+import 'colors';
+import debounce from 'debounce';
+import glob from 'glob';
+import watch from 'node-watch';
+import path from 'path';
+import shell from 'shelljs';
+
+const cwd = process.cwd();
+const config = require(path.join(cwd, 'package.json'));
+
+Object.keys(config.watch).forEach(pattern => {
+  glob(pattern, { cwd }, (err, files) => {
+    if (err) {
+      console.log(err.bold.red);
+    } else {
+      console.log(`Watching ${files.length} files matching '${pattern}'...`.bold);
+      start(config.watch[pattern], config.scripts);
+    }
+
+    watch(files, debounce(filename => {
+      console.log('~'.blue, `${filename} changed`.gray);
+      start(config.watch[pattern], config.scripts);
+    }, 1000));
+  });
+});
+
+function start(watchScripts, npmScripts) {
+  watchScripts.forEach(scriptName => {
+    if (npmScripts[scriptName]) {
+      run(scriptName, npmScripts[scriptName], `npm run ${scriptName} -s`);
+    } else {
+      console.log(`Error: '${scriptName}' not found in scripts`.red, 'check your package.json'.gray);
+    }
+  });
+}
+
+function run(name, script, command) {
+  shell.exec(command, { silent: true }, (exitcode, output) => {
+    if (exitcode === 0) {
+      console.log('✓'.green, name.bold, script.gray);
+    } else {
+      console.log('✗'.red, name.bold, script.gray);
+      console.log(output.trim().gray);
+    }
+  });
+}
